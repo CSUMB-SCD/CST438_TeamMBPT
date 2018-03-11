8a9e16304065b3684106b9251a0f550e82c982c8
Donald Dong <xdong@csumb.edu>
2018-03-11 00:38:18 -0800

    added express route for login

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
+const router = express.Router();
+
+const app = express();
 const __static = path.join(__dirname, '..', '/dist');
 
+app.use(bodyParser.text());
 app.use(express.static(__static));
 app.listen(process.env.PORT || 8080);
+
+// Google user authentication
+router.post('/auth/google', require('./routes/google_auth'));
+// Backend user query
+router.post('/user', require('./routes/user'));
