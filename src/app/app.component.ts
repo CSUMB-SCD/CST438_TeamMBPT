fe7f9612f6933f931e7b30ef1020c69ea3049631
Donald Dong <xdong@csumb.edu>
2018-03-17 18:48:41 -0700

    added login button on the guest page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthGuard} from './services/auth-guard.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Monterey Bay Programming Team';
+  isLoggedIn: boolean;
+
+  constructor(private authGuard: AuthGuard) {
+    this.isLoggedIn = this.authGuard.isLoggedIn;
+  }
+
+  login(): void {
+    this.isLoggedIn = true;
+    this.authGuard.isLoggedIn = true;
+  }
+
+  logout(): void {
+    this.isLoggedIn = false;
+    this.authGuard.isLoggedIn = false;
+  }
+
+  toggleLogin() {
+    if (this.isLoggedIn) {
+      this.logout();
+    } else {
+      this.login();
+    }
+  }
 }
