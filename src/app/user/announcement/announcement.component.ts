913e6d4f72fe57ee5b26797a1a2cf2678ef469e7
Jordan Koelbl <jkoelbl@csumb.edu>
2018-03-19 22:22:57 -0700

    loljk

--- src/app/user/announcement/announcement.component.ts
+++ src/app/user/announcement/announcement.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../services/blog.service';
 
 @Component({
   selector: 'app-announcement',
   templateUrl: './announcement.component.html',
-  styleUrls: ['./announcement.component.css']
+  styleUrls: ['./announcement.component.css'],
+  providers: [BlogService]
+
 })
 export class AnnouncementComponent implements OnInit {
+  blogposts: any;
 
-  constructor() { }
+  constructor(private blogServ: BlogService) {}
 
   ngOnInit() {
+    // sample case; ignore -J
+    this.blogposts = [{'user_id': 'Test User',
+      'date': Date().toString(),
+      'title': 'This is a title',
+      'text': 'This is some text'
+    }];
+    /*this.blogServ.get_blogposts().subscribe(object => {
+      this.blogposts = object;
+    });*/
   }
-
 }
