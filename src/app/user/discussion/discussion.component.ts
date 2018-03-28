4b3bc465222c70ec14264b70e96ef9644503db13
dfajilan <DFajilan@csumb.edu>
2018-03-27 22:37:44 -0700

    Basis for the Discussion page with hardcoded data

--- src/app/user/discussion/discussion.component.ts
+++ src/app/user/discussion/discussion.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {DiscussionService} from '../services/discussion.service';
 
 @Component({
   selector: 'app-discussion',
   templateUrl: './discussion.component.html',
-  styleUrls: ['./discussion.component.css']
+  styleUrls: ['./discussion.component.css'],
+  providers: [DiscussionService]
 })
 export class DiscussionComponent implements OnInit {
+  discussionData: any;
 
-  constructor() { }
+  constructor(private service: DiscussionService) { }
 
   ngOnInit() {
+    this.discussionData =
+      [
+        {
+          'user_name': 'dfajilan',
+          'title': 'Does this thing work',
+          'body': 'Here is a bunch of words I am typing out to test if the subtext will work correctly, hopefully it does!'
+        },
+        {
+          'user_name': 'root',
+          'title': 'This thing does work!',
+          'body': 'Here is a bunch of words I am typing out to test if the subtext will work correctly, hopefully it does!'
+        }
+      ];
+    // this.service.get_Discussion().subscribe(object => {
+    //   this.discussionData = object;
+    // });
   }
-
 }
