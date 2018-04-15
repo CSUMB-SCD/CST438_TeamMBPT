7bead59fb4f3db4c633f598607132006664f0c17
Donald Dong <xdong@csumb.edu>
2018-04-15 11:01:51 -0700

    changed welcome page layout

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  tiles = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+  ];
 
   constructor() { }
 
