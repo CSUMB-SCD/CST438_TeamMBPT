488133bc9ae4ee5610b4c90b4e48b388d1d1dcc6
Donald Dong <xdong@csumb.edu>
2018-04-14 17:19:46 -0700

    added registration form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  firstName = new FormControl();
+  lastName = new FormControl();
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('',
+    [
+      Validators.required,
+      Validators.minLength(8),
+    ]);
+  confirmPassword = new FormControl('',
+    [
+      Validators.required
+  ]);
+  hide_password = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+      this.password.hasError('minlength') ? 'The minimum length of the password is 8 characters.' :
+        '';
+  }
+
+  getEmailErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
 }
